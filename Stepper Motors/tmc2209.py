2be4217489d8d3238058f983476c3fa4af2acf5e
Panithan Lertsuntivit <panithanblue@gmail.com>
2025-08-03 01:41:03 +0800

    Basic stepper motor control

---
+++ Stepper Motors/tmc2209.py
@@ -0,0 +1,28 @@
+from machine import Pin
+from time import sleep_us, sleep
+
+# Define STEP and DIR pins
+step_pin = Pin(15, Pin.OUT)
+dir_pin = Pin(32, Pin.OUT)
+enable_pin = Pin(14, Pin.OUT)
+
+# Enable motor driver
+enable_pin.value(0)		# LOW = enabled
+
+def step_movement(steps, direction=1, delay_us=1000):
+    # direction=1 results in clockwise rotation
+    dir_pin.value(direction)
+    for _ in range(steps):
+        step_pin.on()
+        sleep_us(delay_us)
+        step_pin.off()
+        sleep_us(delay_us)
+        
+# Stepper Motor control
+while True:
+    print("Forward")
+    step_movement(500, direction=1)
+    sleep(1)
+    print("Reverse")
+    step(500, direction=0)
+    sleep(1)
